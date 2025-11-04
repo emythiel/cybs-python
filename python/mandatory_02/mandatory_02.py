093abab83845de25c58d7b3b80ea3d59f47759c6
emythiel <18901520+emythiel@users.noreply.github.com>
2025-11-04 10:45:01 +0100

    docstring raises

--- python/mandatory_02/mandatory_02.py
+++ python/mandatory_02/mandatory_02.py
@@ -24,13 +24,13 @@ DB_TABLE = 'incident_reports'
 def fetch_token(url: str, email: str) -> str:
     """
     Request authentication token from API using a valid student email.
-
     Args:
         url (str): API URL to request from.
         email (str): Student email needed to get a token.
-
     Returns:
         str: The authentication token retrieved from the API.
+    Raises:
+        ValueError: If there was a problem getting a response
     """
 
     headers = {'Content-Type': 'application/json'}
@@ -53,13 +53,13 @@ def fetch_token(url: str, email: str) -> str:
 def fetch_incidents(url: str, token: str) -> dict:
     """
     Fetch incident data from the API using the provided authentication token.
-
     Args:
         url(str): API URL to request from.
         token (str): Bearer authentication token.
-
     Returns:
         dict: Parsed JSON resposne containing incident data.
+    Raises:
+        ValueError: If there was a problem getting a response
     """
 
     headers = {'Authorization': f'Bearer {token}'}
@@ -81,14 +81,13 @@ def fetch_incidents(url: str, token: str) -> dict:
 def db_table_exists(db_path: str, table: str) -> bool:
     """
     Check if a table already exists in the database using provided table.
-    If it does, prompt the user to overwrite or cancel.
-
     Args:
         db_path (str): Path to the database file.
         table (str): Name of the database table.
-
     Returns:
         bool: True if table exists, False if not.
+    Raises:
+        ValueError: If there was a problem checking the database table.
     """
 
     try:
@@ -108,11 +107,12 @@ def db_table_exists(db_path: str, table: str) -> bool:
 
 def db_drop_table(db_path: str, table: str) -> None:
     """
-    Ask to overwrite (delete) provided table, or exit.
-
+    Prompt user to overwrite (delete) provided table, or exit.
     Args:
         db_path (str): Path to the database file.
         table (str): Name of the database table.
+    Raises:
+        ValueError: If there was a problem deleted the database table.
     """
 
     try:
@@ -133,10 +133,11 @@ def db_drop_table(db_path: str, table: str) -> None:
 def create_db_table(db_path: str, table: str) -> None:
     """
     Create a new database table using the provided table name, if it does not already exist.
-
     Args:
         db_path (str): Path to the database file.
         table (str): Name of the database table.
+    Raises:
+        ValueError: If there was a problem creating the database.
     """
 
     try:
@@ -162,11 +163,12 @@ def create_db_table(db_path: str, table: str) -> None:
 def populate_db_table(db_path: str, table: str, incidents: list) -> None:
     """
     Populate the database table with the provided incidents data.
-
     Args:
         db_path (str): Path to the database file.
         table (str): Name of the database table.
         incidents (list): List of incidents (each containing several 'alerts').
+    Raises:
+        ValueError: If there was a problem populating the database table with data.
     """
 
     try:
