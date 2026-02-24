a748339f7f8c503f04d0cd0527b8de866bc3ffb6
emythiel <18901520+emythiel@users.noreply.github.com>
2026-02-24 16:59:54 +0100

    man 01 fix

--- automation-scripting/mandatory_01/weather.py
+++ automation-scripting/mandatory_01/weather.py
@@ -1,9 +1,7 @@
 import logging
-import datetime
 import time
 import os
 
-import geocoder
 import requests
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -16,17 +14,9 @@ logging.basicConfig(
     datefmt='%Y%m%d-%H:%M:%S'
 )
 
-now = datetime.datetime.now()
-
-IP = geocoder.ip("me")
-CITY = IP.city
-
 while True:
-    response = requests.get(f'http://wttr.in/{CITY}?format=3')
+    response = requests.get('http://wttr.in/?format=3')
     response.raise_for_status()
-
-    data = response.text
-
     logger.info(f'Here is current weather: {response.text}')
 
     time.sleep(120)
