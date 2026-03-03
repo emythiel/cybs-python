bc2697de37f7d90e058eb00600269a3468f1e8b3
emythiel <18901520+emythiel@users.noreply.github.com>
2026-03-03 09:57:08 +0100

    wow

--- automation-scripting/mandatory_02/clipboard_logger.ps1
+++ automation-scripting/mandatory_02/clipboard_logger.ps1
@@ -2,31 +2,24 @@
 # Checks clipboard, and compares with stored variable
 # If different, log clipboard and change stored variable to content
 
-$currentClipboard = Get-Clipboard
-
+# Set script path and logfile path
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-$clipboardFile = Join-Path -Path "$scriptPath" -ChildPath "clipboard.txt"
-$logFile = Join-Path -Path "$scriptPath" -ChildPath "clipboard.log"
+$logFile = Join-Path $scriptPath "clipboard.log"
 
-Write-Host "Script path: $scriptPath"
-Write-Host "clipboard file: $clipboardFile"
-
-if (!(Test-Path "$clipboardFile")) {
-    New-Item -Path "$scriptPath" -Name "clipboard.txt"
-    Write-Host "clipboard file not found, creating file"
-} else {
-    Write-Host "clipboard already exists, just continuing"
+# Check if logfile exists, create if it doesn't
+if (!(Test-Path $logFile)) {
+    New-Item -Path $logFile -ItemType File | Out-Null
 }
 
-$fileClipboard = Get-Content -Path "$clipboardFile"
-Write-Host "clipboard.txt content: $fileClipboard"
+# Get current clipboard content
+$currentClipboard = Get-Clipboard
 
+# Get the last line of the logfile, split at first | to ignore timestamp
+$fileClipboard = ((Get-Content $logFile -Tail 1) -split '\|.',2)[1]
+
+# Compare clipboard content with latest logfile entry
 if ($currentClipboard -ne $fileClipboard) {
     # Log the change
     $timestamp = Get-Date
-    Add-Content -Path "$logFile" -Value "$timestamp | $currentClipboard"
-    # Save new clipboard content
-    Set-Content -Path "$clipboardFile" -Value "$currentClipboard"
-} else {
-    Write-Host "Nice"
+    Add-Content -Path $logFile -Value "$timestamp | $currentClipboard"
 }
