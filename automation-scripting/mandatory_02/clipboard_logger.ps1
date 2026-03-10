642bbef2478c111ea6e9cf48458fd6c89adbaee3
emythiel <18901520+emythiel@users.noreply.github.com>
2026-03-10 12:24:02 +0100

    dnwoa

--- automation-scripting/mandatory_02/clipboard_logger.ps1
+++ automation-scripting/mandatory_02/clipboard_logger.ps1
@@ -1,25 +1,35 @@
 # Clipboard Logger
 # Checks clipboard, and compares with stored variable
 # If different, log clipboard and change stored variable to content
+# Install (Powershell, run as admin):
+# schtasks /create /sc ONLOGON /tn Clippy /tr "powershell.exe -ExecutionPolicy Bypass -File C:\PATH\TO\FILES\clipboard_logger.ps1"
 
 # Set script path and logfile path
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 $logFile = Join-Path $scriptPath "clipboard.log"
 
-# Check if logfile exists, create if it doesn't
-if (!(Test-Path $logFile)) {
-    New-Item -Path $logFile -ItemType File | Out-Null
-}
+while ($true) {
+    # Check if logfile exists, create if it doesn't
+    # Done inside loop in case file is deleted while script is running
+    if (!(Test-Path $logFile)) {
+        New-Item -Path $logFile -ItemType File | Out-Null
+    }
 
-# Get current clipboard content
-$currentClipboard = Get-Clipboard
+    # Get current clipboard content
+    $currentClipboard = Get-Clipboard
 
-# Get the last line of the logfile, split at first | to ignore timestamp
-$fileClipboard = ((Get-Content $logFile -Tail 1) -split '\s\|\s',2)[1]
+    # Get last line of clipboard (if it's there)
+    $lastLine = Get-Content $logFile -Tail 1 -ErrorAction SilentlyContinue
+    # Split at '|' so we ignore  the initial timestamp
+    $fileClipboard = if ($lastLine) { ($lastLine -split '\s\|\s',2)[1] } else { "" }
 
-# Compare clipboard content with latest logfile entry
-if ($currentClipboard -ne $fileClipboard) {
-    # Log the change
-    $timestamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-    Add-Content -Path $logFile -Value "$timestamp | $currentClipboard"
+    # Compare clipboard content with latest logfile entry
+    if ($currentClipboard -ne $fileClipboard) {
+        # Log the change
+        $timestamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+        Add-Content -Path $logFile -Value "$timestamp | $currentClipboard"
+    }
+
+    # Wait 5 seconds before checking again
+    Start-Sleep -Seconds 5
 }
